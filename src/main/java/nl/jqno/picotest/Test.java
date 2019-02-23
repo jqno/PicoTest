32bc4a311da4fc122a49129e3eb846552ed475c0
Jan Ouwens <jan.ouwens@gmail.com>
2019-02-23 20:41:49 +0100

    Makes IntelliJ recognise PicoTests as runnable test classes

--- src/main/java/nl/jqno/picotest/Test.java
+++ src/main/java/nl/jqno/picotest/Test.java
@@ -1,12 +1,14 @@
 package nl.jqno.picotest;
 
 import nl.jqno.picotest.engine.TestCollector;
+import org.junit.platform.commons.annotation.Testable;
 
 public abstract class Test {
 
     public abstract void fixture();
     private TestCollector collector = null;
 
+    @Testable
     public final void test(String description, Runnable test) {
         collector.accept(description, test);
     }
